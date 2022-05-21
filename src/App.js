4d5383d414d9d13a8b2fad265d57d7a269c8fd08
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-05-21 18:11:24 -0500

    still needs styled components and transitions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
+
+import Character from "./components/Character.js"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +11,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // set state with our data from the server
+  // we need a container to put the character cards in
+  // we need a character component that will return an individual character card
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setData(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="card-container">
+        { data.data
+          ? data.data.map((each, index) => <Character data={each} key={index}/>)
+          : <h3>Loading...</h3>
+        }
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React, { useRef } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
+
+export default function Character({ data }){
+
+    const infoRef = useRef(null);
+
+    const display = () => {
+        infoRef.current.classList.toggle("hide");
+    }
+
+    return (
+        <div className="card">
+            <div className="card-header">
+                <h3>{`${data.name}`}</h3>
+               <button onClick={display}><FontAwesomeIcon icon={faAngleDown} size="xl"/></button>
+            </div>
+            
+            <div className="card-info hide" ref={infoRef}>
+                <p>{`Birth Year: ${data.birth_year}`}</p>
+                <p>{`Height: ${data.height}`}</p>
+                <p>{`Mass: ${data.mass}`}</p>
+                <p>{`Eye Color: ${data.eye_color}`}</p>
+                <p>{`Hair Color: ${data.hair_color}`}</p>
+                <p>{`Skin Color: ${data.skin_color}`}</p>
+            </div>
+            
+        </div>
+    )
+
+}
