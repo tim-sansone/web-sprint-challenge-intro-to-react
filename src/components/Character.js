3eb12a94d7e328763f817983b809fe52614aeca6
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-05-21 22:08:47 -0500

    styled components not working yet

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,24 +1,55 @@
 // Write your Character component here
 import React, { useRef } from "react"
+import styled from "styled-components"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
+
+const StyledCard = styled.div`
+    width: 95%;
+    background-color: rgb(103, 171, 216, 0.8);
+    border: 1px solid black;
+    border-radius: 10px;
+    color: black;
+    text-shadow: 1px 1px 5px #fff;
+    margin-bottom: 2%;
+    overflow: hidden;
+    height: 3.6rem;
+    transition: all 0.5s
+    `
+
+const StyledCardHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 2%;
+`
+
+const StyledButton = styled.button`
+    background-color: inherit;
+    border: none;
+    transition: all 0.5s;
+`
+
+
 
 export default function Character({ data }){
 
     const infoRef = useRef(null);
+    const buttonRef = useRef(null);
 
-    const display = () => {
-        infoRef.current.classList.toggle("hide");
+    const display = (event) => {
+        infoRef.current.classList.toggle("unhide");
+        buttonRef.current.classList.toggle("up");
+        console.log(buttonRef);
     }
 
     return (
-        <div className="card">
-            <div className="card-header">
+        <StyledCard ref={infoRef}>
+            <StyledCardHeader>
                 <h3>{`${data.name}`}</h3>
-               <button onClick={display}><FontAwesomeIcon icon={faAngleDown} size="xl"/></button>
-            </div>
+               <StyledButton onClick={display} ref={buttonRef}><FontAwesomeIcon icon={faAngleDown} size="xl" /></StyledButton>
+            </StyledCardHeader>
             
-            <div className="card-info hide" ref={infoRef}>
+            <div className="card-info" >
                 <p>{`Birth Year: ${data.birth_year}`}</p>
                 <p>{`Height: ${data.height}`}</p>
                 <p>{`Mass: ${data.mass}`}</p>
@@ -27,7 +58,7 @@ export default function Character({ data }){
                 <p>{`Skin Color: ${data.skin_color}`}</p>
             </div>
             
-        </div>
+        </StyledCard>
     )
 
 }
