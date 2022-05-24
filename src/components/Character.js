3597b1b15811100b00fef9497c74e43264ac56df
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-05-24 09:02:58 -0500

    yes that is my final answer

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,30 +4,30 @@ import styled from "styled-components"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
 
-const StyledCard = styled.div`
-    width: 95%;
-    background-color: rgb(103, 171, 216, 0.8);
-    border: 1px solid black;
-    border-radius: 10px;
-    color: black;
-    text-shadow: 1px 1px 5px #fff;
-    margin-bottom: 2%;
-    overflow: hidden;
-    height: 3.6rem;
-    transition: all 0.5s
-    `
+// const StyledCard = styled.div`
+//     width: 95%;
+//     background-color: rgb(103, 171, 216, 0.8);
+//     border: 1px solid black;
+//     border-radius: 10px;
+//     color: black;
+//     text-shadow: 1px 1px 5px #fff;
+//     margin-bottom: 2%;
+//     overflow: hidden;
+//     height: 3.6rem;
+//     transition: all 0.5s
+//     `
 
-const StyledCardHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 0 2%;
-`
+// const StyledCardHeader = styled.div`
+//     display: flex;
+//     justify-content: space-between;
+//     padding: 0 2%;
+// `
 
-const StyledButton = styled.button`
-    background-color: inherit;
-    border: none;
-    transition: all 0.5s;
-`
+// const StyledButton = styled.button`
+//     background-color: inherit;
+//     border: none;
+//     transition: all 0.5s;
+// `
 
 
 
@@ -43,11 +43,11 @@ export default function Character({ data }){
     }
 
     return (
-        <StyledCard ref={infoRef}>
-            <StyledCardHeader>
+        <div className="card" ref={infoRef}>
+            <div className="card-header">
                 <h3>{`${data.name}`}</h3>
-               <StyledButton onClick={display} ref={buttonRef}><FontAwesomeIcon icon={faAngleDown} size="xl" /></StyledButton>
-            </StyledCardHeader>
+               <button onClick={display} ref={buttonRef}><FontAwesomeIcon icon={faAngleDown} size="xl" /></button>
+            </div>
             
             <div className="card-info" >
                 <p>{`Birth Year: ${data.birth_year}`}</p>
@@ -58,7 +58,7 @@ export default function Character({ data }){
                 <p>{`Skin Color: ${data.skin_color}`}</p>
             </div>
             
-        </StyledCard>
+        </div>
     )
 
 }
